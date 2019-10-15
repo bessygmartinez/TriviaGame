2de603b725f501b0356301d7fe1e07d94faf7317
Bessy Gee <nakiwarai13@gmail.com>
2019-10-15 16:50:02 -0400

    added document ready and click function to start game

---
+++ assets/javascript/app.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+
+    //Starts game when #start button clicked
+    $("#start").on("click", function () {
+        console.log("Start button clicked");
+    });
+
+});
